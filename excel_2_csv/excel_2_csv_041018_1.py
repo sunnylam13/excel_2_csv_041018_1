d0596491f635f3737dc7077741a8a4271fe7b661
Sunny Lam <sunny.lam@gmail.com>
2018-04-10 11:25:10 -0400

    removed previous test data, added different ones

--- excel_2_csv/excel_2_csv_041018_1.py
+++ excel_2_csv/excel_2_csv_041018_1.py
@@ -3,11 +3,35 @@
 #! python3
 
 # USAGE
-# python3 excel_2_csv_041018_1.py
+# python3 excel_2_csv_041018_1.py "FOLDERWITHEXCEL"
 
-import os,sys,csv
+import os,sys,csv,openpyxl
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
 # logging.disable(logging.CRITICAL)
+
+#####################################
+# COMMAND LINE PARSING
+#####################################
+
+input_folder = sys.argv[1] # should be "FOLDERWITHEXCEL", which should be a path
+logging.debug('The input folder targeted is:  %s' % (input_folder))
+
+#####################################
+# END COMMAND LINE PARSING
+#####################################
+
+#####################################
+# ANALYZE FILES
+#####################################
+
+for file in os.listdir(input_folder):
+	# check that file is an Excel file
+	if endswith('xlsx'):
+		pass
+
+#####################################
+# END ANALYZE FILES
+#####################################
 
